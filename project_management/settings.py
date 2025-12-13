84ec84711ef658c3bab8a11f0be575021d343297
feteneabebe07-sketch <feteneabebe07@gmail.com>
2025-12-13 08:23:02 +0300

    Refactor database configuration to use env variable
    Updated DATABASE_URL retrieval to use environment variable.

--- project_management/settings.py
+++ project_management/settings.py
@@ -138,10 +138,14 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'mysql://devuser:password123@localhost:3307/tasks'),
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
+        ssl_require=True,
     )
 }
 
